b69f5a778eb60852a9032470a189b460be07a460
bestdtang <94884915+MinPan9585@users.noreply.github.com>
2023-01-12 09:51:39 +0800

    Course 0112

---
+++ Pixelated Palette/Assets/Scripts/GameManagerf.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerf : MonoBehaviour
+{
+    private void OnMouseEnter()
+    {
+        
+    }
+
+    private void OnMouseExit()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        
+    }
+
+    private void Update()
+    {
+        //if (Input.GetKeyDown(KeyCode.Mouse0)) 
+        //{
+        //    Physics2D.Raycast();
+        //}
+    }
+}
